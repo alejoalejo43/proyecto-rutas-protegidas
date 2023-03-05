83670a67c7909783e983ee333ba36292ad35ca2a
Oscar Alejandro Castillo Verano <55119670+alejoalejo43@users.noreply.github.com>
2023-03-04 21:38:18 -0500

    Proyecto finalizado

--- src/auth/auth.controllers.js
+++ src/auth/auth.controllers.js
@@ -1,7 +1,18 @@
-
+const { findUserByEmail } = require('../users/users.controllers');
+const { comparePassword } = require('../utils/crypto');
 
 const checkUsersCredentials = async (email, password) => {
-
-}
-
-module.exports = checkUsersCredentials
+  try {
+    const user = await findUserByEmail(email);
+    const verifyPassword = comparePassword(password, user.password);
+    if (verifyPassword) {
+      return user;
+    } else {
+      false;
+    }
+  } catch (error) {
+    console.log('error');
+    throw new Error(error);
+  }
+};
+module.exports = { checkUsersCredentials };
--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,12 +1,12 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const jwtPassport = require('../middlewares/auth.middleware');
+const userServices = require('./users.services');
 
-const userServices = require('./users.services')
+router.get('/', userServices.getAllUsers);
+router.post('/', userServices.postNewUser);
 
-router.get('/', userServices.getAllUsers)
-router.post('/', userServices.postNewUser)
+router.get('/:id', userServices.getUserById);
+router.patch('/:id', jwtPassport, userServices.patchUser);
+router.delete('/:id', jwtPassport, userServices.deleteUser);
 
-router.get('/:id', userServices.getUserById)
-router.patch('/:id', userServices.patchUser)
-router.delete('/:id', userServices.deleteUser)
-
-module.exports = router
+module.exports = router;
